5608065e3df891911c4000554e4f782fc372a8d3
mishasvb777 <mishasvb777@gmail.com>
2024-10-09 12:41:20 +0300

    test server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const getHomePage = async () => {
-  const res = await fetch('https://lm-test-page.ru/home')
+  const res = await fetch('https://localhost:3000/home')
   const data = await res.json()
   return data
 }
